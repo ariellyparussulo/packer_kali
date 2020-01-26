038b03ab2302244cc412ba3cbc4ffaaf0b4da8ac
Arielly <ariellyblabla@gmail.com>
2020-01-26 11:35:32 -0300

    Rename default user, add sshd configuration and update preseed.cfg

---
+++ scripts/sshd.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -eux
+
+echo "UseDNS no" >>/etc/ssh/sshd_config;
+echo "GSSAPIAuthentication no" >>/etc/ssh/sshd_config;
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config;
+
+systemctl restart sshd
